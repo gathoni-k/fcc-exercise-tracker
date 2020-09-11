9ca57bd03964986c68598622bcbc2078b7b8a349
gathoni-k <maryg.kahiga@gmail.com>
2020-09-11 11:18:33 +0300

    chore: Refactor exercise into Exercise subcollection

--- models/user.model.js
+++ models/user.model.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
-const formatDate = () => {
-  const event = new Date(Date.now())
-  const options = { year: 'numeric', month: 'numeru=ic', day: 'numeric' }
-  const date = event.toLocaleDateString(options)
-  return date.split('/').join('-')
-}
+
 const UserSchema = new Schema({
   username: String,
-  description: String,
-  duration: String,
-  date: {
-    type: Date,
-    default: formatDate()
-  }
+  exercises: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Exercise'
+  }]
 })
 module.exports = mongoose.model('User', UserSchema)
